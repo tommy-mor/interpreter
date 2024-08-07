8fc553e254764fec76fa9ee457c36e12fcff8d50
tommy-mor <thmorriss@gmail.com>
2024-08-06 18:58:58 -0700

    basic tests

--- src/tommy_mor/interpreter.clj
+++ src/tommy_mor/interpreter.clj
@@ -1,25 +1,59 @@
 (ns tommy-mor.interpreter
+  (:require [clojure.string :as str]
+            [hyperfiddle.rcf :refer [tests]])
   (:gen-class))
 
+(hyperfiddle.rcf/enable!)
+
+
+
+(defn compile-expr [tape [expr & program]]
+  (cond (nil? expr)
+        `(last (deref ~tape))
+        
+        (and (symbol? expr)
+             (str/starts-with? (name expr) "!"))
+        
+        `(do (swap! ~tape conj ~expr)
+             ~(compile-expr tape program))
+        
+        (or (string? expr) (number? expr))
+        `(do (swap! ~tape conj ~expr)
+             ~(compile-expr tape program))
+        
+        true
+        (throw (ex-info "unknown form" {:form expr}))))
 
 (defn compile [program]
   (let [tape (gensym 'tape)]
     `(let [~tape (atom [])]
-       ~@(for [expr program]
-           (cond (symbol? expr) `(swap! ~tape conj ~expr)
-                 true (println 'unknown (type expr)))))))
+       ~(compile-expr tape program))))
 
 (defmacro defstackfn [name args & program]
   (let [result (compile program)]
     `(defn ~name ~args ~result)))
 
-(macroexpand '(defstackfn f [!a] !a 2 3 4))
-(def f (clojure.core/fn ([!a] (clojure.core/let [tape3430 (clojure.core/atom [])] (clojure.core/swap! tape3430 clojure.core/conj !a) nil nil nil))))
-(f 1)
+(tests
+ "constants"
+ (do (defstackfn f [] 3) (f)) := 3
+ 
+ (do (defstackfn f [] "ars") (f)) := "ars"
+ 
+ (do (defstackfn f [] 3 4) (f)) := 4
+ 
+ (do (defstackfn f [!a] !a) (f 3)) := 3
+ (do (defstackfn f [!a] 3 !a) (f 4)) := 4
+ 
+ ;; (defstackfn f [!a] !a !b) :throws java.lang.RuntimeException ;; rcf cant catch at macroexpransion time I guess
+ 
+ (do (defstackfn f [!a !b] !b) (f 2 4)) := 4
 
-(def f (fn [!a]
-         (let [tommy-mor.interpreter/tape (clojure.core/atom [])]
-           (swap! tape conj (clojure.core/unquote expr))
-           nil
-           nil
-           nil)))
+
+ )
+
+
+(defstackfn f [!a] !a 2 3 4)
+
+(f 2)
+
+
